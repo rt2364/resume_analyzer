91aad3bc67dd68b676e3bbd56662fc4649f81e59
CodeSeeker <163332011+rt2364@users.noreply.github.com>
2026-08-30 15:49:33 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://localhost:5000/api',
+  baseURL: baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
 });
 
 // Automatic Token Interceptor
